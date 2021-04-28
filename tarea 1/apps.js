f3c62ed293f5b506f4c8d83144d6f795eb1249cd
julio zarate <juliochetoz@gmail.com>
2021-04-28 19:45:11 -0300

    first commit

---
+++ tarea 1/apps.js
@@ -0,0 +1,13 @@
+let numero_1 = 20
+
+let numero_2 = 15
+
+console.log(numero_1 + numero_2)
+console.log(numero_1 - numero_2)
+console.log(numero_1 * numero_2)
+console.log(numero_1 / numero_2)
+console.log(numero_1 % numero_2)
+
+console.log(numero1 != numero2);
+console.log(numero1 >= numero2);
+console.log(numero1 <= numero2)
