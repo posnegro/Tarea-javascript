054974a020e06708c69d33f273cd93b7e8b1113b
julio zarate <juliochetoz@gmail.com>
2021-05-19 12:42:50 -0300

    commit seven

--- tarea 5/dados/apps.js
+++ tarea 5/dados/apps.js
@@ -3,14 +3,14 @@ let anotador = []
 function dados(min , max){
     return Math.floor(Math.random() * (max - min) + min) ;
 }
-let primero = dados(1,6)
-let segundo = dados(1,6)
+
+let primero = dados(1,7)
+let segundo = dados(1,7)
 let resultado = primero + segundo
 console.log(`${primero} + ${segundo} = ${resultado} `)
 console.log("----------------------------------")
     
 anotador.push(primero + segundo)
-
 
 for(i=0; i<=50; i++){
 
@@ -22,8 +22,6 @@ for(i=0; i<=50; i++){
     else{
         console.log(`${i + 1} - ${random}`)
     }
-
-
 
 }
 console.log(anotador)
