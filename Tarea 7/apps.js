31d941c3408370af35aedc6ef2136a81b1c413d5
julio zarate <juliochetoz@gmail.com>
2021-05-28 18:55:59 -0300

    nine commit

--- Tarea 7/apps.js
+++ Tarea 7/apps.js
@@ -1,41 +1,68 @@
 let carrito =[]
-
 class Productos{
-    constructor(codigo, nombre, precio){
-        this.codigo = codigo
-        this.nombre = nombre
-        this.precio = precio
-
+     constructor(codigo , nombre , precio ){
+        this.codigo = codigo 
+        this.nombre = nombre 
+        this.precio = precio 
+        this.i = 001
 
     }
 
     imprimeDatos(){
-        console.log(`El ${this.nombre} es el producto N°${i+1} `)
+        console.log(`El ${this.nombre} es el producto N° `)
         console.log(`El codigo de barra es ${this.codigo}`)
         console.log(`Su precio ${this.precio}`)
     }
 
-
-}
-for(i=0; i < 3; i++){
-
-    
-    let nombre = prompt(`Introduzca el nombre del producto N°${i+1}`)
-        while(nombre == null || nombre == ""){        
-        nombre = prompt(`Vuelva el nombre del producto N°${i+1}`)}
-    
-    let codigo = parseInt(prompt("codigo del producto"))
-      while(isNaN(codigo)){
-        codigo = parseInt(prompt("vuelva a introducir el codigo del producto"))         
-    }
-    let precio = parseInt(prompt("precio del producto"))
-      while(isNaN(precio)){
+    crearProducto(){
         
-         precio = parseInt(prompt("vuelva a introducir el precio del producto"))         
+        this.nombre = prompt(`Introduzca el nombre del producto N°$`)
+        while(this.nombre == null ||this.nombre == ""){        
+         this.nombre = prompt(`Vuelva el nombre del producto N°$`)
+        }
+        this.codigo = parseInt(prompt("codigo del producto"))
+        while(isNaN(this.codigo)){
+            this.codigo = parseInt(prompt("vuelva a introducir el codigo del producto"))         
+        }
+         this.precio = parseInt(prompt("precio del producto"))
+         while(isNaN( this.precio)){
+            this.precio = parseInt(prompt("vuelva a introducir el precio del producto"))
+        }
+        carrito.push(item)
+         }
     
-}
-    let item = new Productos(codigo, nombre, precio)
-    item.imprimeDatos()
-    carrito.push(item)
-}
+
+    }
+let item = new Productos(this.codigo, this.nombre, this.precio)
+
+
+item.crearProducto()
+item.imprimeDatos()
+ 
+
+
+
+    // 4{
+
+
+    //     let nombre = prompt(`Introduzca el nombre del producto N°${i+1}`)
+    //         while(nombre == null || nombre == ""){        
+    //         nombre = prompt(`Vuelva el nombre del producto N°${i+1}`)}
+        
+    //     let codigo = parseInt(prompt("codigo del producto"))
+    //       while(isNaN(codigo)){
+    //         codigo = parseInt(prompt("vuelva a introducir el codigo del producto"))         
+    //     }
+    //     let precio = parseInt(prompt("precio del producto"))
+    //       while(isNaN(precio)){
+            
+    //          precio = parseInt(prompt("vuelva a introducir el precio del producto"))         
+        
+    // }
+    //     let item = new Productos(codigo, nombre, precio)
+    //     item.imprimeDatos()
+    //     carrito.push(item)
+    // }
+    
+    
 
---
+++ Tarea 7/apps2.js
@@ -0,0 +1,37 @@
+let carrito =[];
+class Productos{
+     constructor(i, nombre, precio){
+        
+         
+        this.i = i
+        this.nombre = nombre = prompt(`Introduzca el nombre del producto `);
+        while(this.nombre == null ||this.nombre == ""){        
+         this.nombre = prompt(`Vuelva el nombre del producto`);
+        }
+        this.precio = precio = parseInt(prompt("precio del producto"));
+        while(isNaN( this.precio)){
+           this.precio = parseInt(prompt("vuelva a introducir el precio del producto"));
+       }
+        this.item = [this.i, this.nombre, this.precio];
+       carrito.push(this.item);
+    
+}
+
+    imprimeDatos(){
+        console.log(`agrego ${this.nombre} al carrito `)
+        console.log(`El codigo de barra es $0${this.i+1}`)
+        console.log(`Su precio ${this.precio}`)
+    };
+       
+
+    }
+function cajaRegistradora(){  
+
+    for(i=0; i < 3; i++){
+
+        item = new Productos(i)
+
+        item.imprimeDatos()
+}
+};
+cajaRegistradora()
